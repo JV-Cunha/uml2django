387e9cdfaf760a8aa04da5ffa9cb59cbc5cb3d9c
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-05-17 13:51:10 -0300

    added more templates

--- src/uml2django/templates/__init__.py
+++ src/uml2django/templates/__init__.py
@@ -4,22 +4,21 @@ import os
 from uml2django import templates
 
 
-def getTemplatePath(filename: str) -> str:
+def getTemplatePath(filename: str,directory = "") -> str:
     path = os.path.dirname(templates.__file__)
-    template_path = os.path.join(path, filename)
+    template_path = os.path.join(path, directory, filename)
     return template_path
 
 def getViewsTemplatePath(filename: str) -> str:
-    path = os.path.dirname(templates.__file__)
-    template_path = os.path.join(path, "views", filename)
-    return template_path
+    return getTemplatePath(filename, "views")
+
 
 
 BASE_MODEL_TEMPLATE_PATH = getTemplatePath("base_model.tmpl")
 MODEL_TEMPLATE_PATH = getTemplatePath("model.tmpl")
 
-CREATE_VIEW_TEMPLATE_PATH = getViewsTemplatePath("create.tmpl")
-DELETE_VIEW_TEMPLATE_PATH = getViewsTemplatePath("delete.tmpl")
-DETAIL_VIEW_TEMPLATE_PATH = getViewsTemplatePath("detail.tmpl")
-LIST_VIEW_TEMPLATE_PATH = getViewsTemplatePath("list.tmpl")
-UPDATE_VIEW_TEMPLATE_PATH = getViewsTemplatePath("update.tmpl")
+CREATE_VIEW_TEMPLATE_PATH = getViewsTemplatePath("CreateView.tmpl")
+DELETE_VIEW_TEMPLATE_PATH = getViewsTemplatePath("DeleteView.tmpl")
+DETAIL_VIEW_TEMPLATE_PATH = getViewsTemplatePath("DetailView.tmpl")
+LIST_VIEW_TEMPLATE_PATH = getViewsTemplatePath("ListView.tmpl")
+UPDATE_VIEW_TEMPLATE_PATH = getViewsTemplatePath("UpdateView.tmpl")
