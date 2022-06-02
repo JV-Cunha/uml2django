732d8b61d76942f84a92f6c0c88f275be873b6bb
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-02 18:10:46 -0300

    configure project files

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 """
     Setup file for uml2django.
     Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 4.2.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
 """
 from setuptools import setup
 
