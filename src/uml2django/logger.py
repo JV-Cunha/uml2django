6b230a088440d9a13f0fd159c74ccb2cffef5e65
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-15 16:42:41 -0300

    change logformat

--- src/uml2django/logger.py
+++ src/uml2django/logger.py
@@ -11,7 +11,8 @@ def setup_logging(loglevel):
     Args:
       loglevel (int): minimum loglevel for emitting messages
     """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s: %(message)s"
+    # logformat = "[%(asctime)s] %(levelname)s:%(name)s: %(message)s"
+    logformat = "[%(asctime)s] %(levelname)s: %(message)s"
     logging.basicConfig(
         level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
     )
