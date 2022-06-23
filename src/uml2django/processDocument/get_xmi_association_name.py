f6cdfe729bebd37bb82428a3292c388ca85e1b56
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-23 16:31:16 -0300

    if association has no  name do nothing

--- src/uml2django/processDocument/get_xmi_association_name.py
+++ src/uml2django/processDocument/get_xmi_association_name.py
@@ -6,7 +6,7 @@ def get_xmi_association_name(association_element: minidom.Element):
         "name"
     ).value if association_element.hasAttribute(
         "name"
-    ) else None
-    if not association_name:
-        raise AttributeError("Association must have a name attribute")
+    ) else ""
+    # if not association_name:
+        # raise AttributeError("Association must have a name attribute")
     return association_name
