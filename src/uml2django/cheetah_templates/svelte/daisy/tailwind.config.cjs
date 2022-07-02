6826d27cae74d7a577e8d7785d8835b4a590963d
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-07-01 23:54:04 -0300

    svelte menu update

--- src/uml2django/cheetah_templates/svelte/daisy/tailwind.config.cjs
+++ src/uml2django/cheetah_templates/svelte/daisy/tailwind.config.cjs
@@ -1,4 +1,14 @@
 module.exports = {
   content: ['./src/routes/**/*.{svelte,js,ts}','./src/lib/**/*.{svelte,js,ts}'],
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["corporate","halloween",],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "halloween",
+  },
 }
